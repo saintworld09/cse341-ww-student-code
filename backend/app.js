db463997cf136525a1d32f674e0545cdc6e92d0d
saintworld09 <ababatunde306@gmail.com>
2026-07-03 12:43:46 +0100

    individual project practice

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const professionalRoutes = require('./routes/professional');
 
--- backend/controllers/professional.js
+++ backend/controllers/professional.js
@@ -1,11 +1,25 @@
 const mongodb = require('../db/connect');
 
-const getData = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection('user').find();
-  result.toArray().then((lists) => {
+const getData = async (req, res) => {
+  try {
+    const result = await mongodb
+      .getDb()
+      .db("WebSevicesProject")
+      .collection("user")
+      .findOne({}, { projection: { base64Image: 0 } });
+
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]); // we just need the first one (the only one)
-  });
+    res.status(200).json(result);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      message: 'An error occurred while retrieving the data.',
+      error: err.message
+    });
+  }
 };
 
-module.exports = { getData };
+module.exports = {
+  getData
+};
